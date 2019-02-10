8a1d7aac5777d837147baa1b27c2a0ea64a21a78
Ferdinand <ferd656@gmail.com>
2019-02-09 20:18:28 -0600

    Third commit

--- tablapostgre(python2x_3x).py
+++ tablapostgre(python2x_3x).py
@@ -1,3 +1,4 @@
+# coding=utf-8
 """
 +-------------------------------------------------------------------------------+
 | MODULO DE CLASE PARA EL MANEJO DE TABLAS POSTGRESQL                           |
